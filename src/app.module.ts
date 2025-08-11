862214a93feb60e036b17d14675e306319e05253
KittyWind <nicolasasperatti@gmail.com>
2025-08-11 16:16:10 -0300

    generando prisma module y service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModeloModule } from './modelo/modelo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ModeloModule],
+  imports: [ModeloModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
