573ea27f9d6d575c3359d20fcaab7473bd2bb3f6
KittyWind <nicolasasperatti@gmail.com>
2025-08-14 12:48:32 -0300

    creating empty producto resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModeloModule } from './modelo/modelo.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductoModule } from './producto/producto.module';
 
 @Module({
-  imports: [ModeloModule, PrismaModule],
+  imports: [ModeloModule, PrismaModule, ProductoModule],
   controllers: [AppController],
   providers: [AppService],
 })
